3a894f4076b7f3e69fa25339738c15a1cd9ac605
SV <sv@SVs-MacBook-Air.local>
2017-03-02 14:41:46 +0100

    new: table convertor

--- parse_table.py
+++ parse_table.py
@@ -8,6 +8,8 @@ Created on Mar 2, 2017
     <svitlana.vakulenko@gmail.com>
 
 '''
+import unittest
+
 import os
 import pandas as pd
 
@@ -36,34 +38,44 @@ def read_tables(fps, delimiter, shuffle=False, limit=False):
     return tables
 
 
-def test_read_tables(files):
+def collect_tables(files):
     # collect file paths
     fps = []
     for file in files:
         fps.append(os.path.join(PATH, file))
     print fps
-    tables = read_tables(fps, delimiter=';')
+    return read_tables(fps, delimiter=';')
+
+
+def generate_data(table, out_path='./data/table_data.txt'):
+    with open(out_path, 'w') as file:
+        columns = table.columns.values
+        print columns
+        for row in table.itertuples():
+            data_string = str(row[0]+1) + ' '
+            # print row
+            values = []
+            for idx, value in enumerate(row[1:]):
+                values.append(str(columns[idx]) + ' : ' + str(value))
+            data_string += ', '.join(values) + ' .\n'
+            file.write(data_string)
+
+
+def test_generate_data():
+    tables = collect_tables([SAMPLE_TABLE])
     for path, table in tables.items():
         print path
-        print table.columns.values
+        generate_data(table)
 
 
-def generate_dataset(table):
-    columns = table.columns.values
-    print columns
-
-
-def test_generate_dataset(files):
-    # collect file paths
-    fps = []
-    for file in files:
-        fps.append(os.path.join(PATH, file))
-    print fps
-    tables = read_tables(fps, delimiter=';')
-    for path, table in tables.items():
-        print path
-        generate_dataset(table)
+class TestParseTable(unittest.TestCase):
+    def test_collect_tables(self):
+        tables = collect_tables([SAMPLE_TABLE])
+        for path, table in tables.items():
+            print path
+            print table.columns.values
 
 
 if __name__ == '__main__':
-    test_generate_dataset([SAMPLE_TABLE])
+    # unittest.main()
+    test_generate_data()
